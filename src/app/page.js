5d741c755bfc0d6001d2e04b4b856abc21abf7ee
Hazimi Asyraf <jimmyasyraf@gmail.com>
2025-01-15 16:22:19 +0800

    Add contact form

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,8 @@ import {SquareArrowOutUpRight} from 'lucide-react';
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
 import projects from "@/data/projects.json";
 import archivedProjects from "@/data/archivedProjects.json";
 import contributions from "@/data/contributions.json";
@@ -19,13 +21,13 @@ const ProjectCard = ({title, description, logo, photo, imageWidth, imageHeight, 
     <CardHeader>
       <div className="flex flex-row w-full items-center">
         <div>
-        <Image
-          className="rounded-md border border-zinc-200"
-          src={logo}
-          alt={title}
-          width={40}
-          height={40}
-        />
+          <Image
+            className="rounded-md border border-zinc-200"
+            src={logo}
+            alt={title}
+            width={40}
+            height={40}
+          />
         </div>
         <h2 className={`ml-4 font-semibold text-base`}>{title}</h2>
         {link && (
@@ -68,9 +70,8 @@ const ContributionCard = ({title, description, link}) => (
 const ExperienceCard = ({company, position, tenure, description}) => (
   <Card>
     <CardContent className='p-6'>
-
-        <h2 className={`font-semibold text-base`}>{company}</h2>
-        <span className="font-normal text-gray-700 text-sm">{tenure}</span>
+      <h2 className={`font-semibold text-base`}>{company}</h2>
+      <span className="font-normal text-gray-700 text-sm">{tenure}</span>
 
       <Separator className='my-2'/>
 
@@ -93,10 +94,9 @@ export default function Home() {
       <h1 className={`${spaceGrotesk.className} font-semibold text-3xl mt-4`}>Software Engineer, Robotics Enthusiast</h1>
       <p className="text-gray-700 mt-4 text-base">I'm a full-stack software engineer based in Ottawa, Canada who enjoys putting things together and make things work. My favorite work lies at the intersection of design and development, creating experiences that look great and performant.</p>
       <p className="text-gray-700 mt-4 text-base">In the past, I've had the opportunity to develop software across a variety of domains, from financial platforms to geospatial tools. My main tools are Ruby on Rails and React.</p>
-      <Button variant="default" className="mt-4">Let's work</Button>
 
       <section>
-        <h1 className={`font-semibold text-xl mt-8 mb-4`}>Projects</h1>
+        <h1 className={`font-semibold text-2xl mt-8 mb-4`}>Projects</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           {projects.map((project, i) => (
             <ProjectCard
@@ -114,7 +114,7 @@ export default function Home() {
       </section>
 
       <section>
-        <h1 className={`font-semibold text-xl mt-8 mb-4`}>Archived Projects</h1>
+        <h1 className={`font-semibold text-2xl mt-8 mb-4`}>Archived Projects</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           {archivedProjects.map((project, i) => (
             <ProjectCard
@@ -132,7 +132,7 @@ export default function Home() {
       </section>
 
       <section>
-        <h1 className={`font-semibold text-xl mt-8 mb-4`}>Open Source Contributions</h1>
+        <h1 className={`font-semibold text-2xl mt-8 mb-4`}>Open Source Contributions</h1>
         <div className="flex flex-col gap-4">
           {contributions.map((contribution, i) => (
             <ContributionCard
@@ -146,7 +146,7 @@ export default function Home() {
       </section>
 
       <section>
-        <h1 className={`font-semibold text-xl mt-8 mb-4`}>Experience</h1>
+        <h1 className={`font-semibold text-2xl mt-8 mb-4`}>Experience</h1>
         <div className="flex flex-col gap-4">
           {experience.map((ex, i) => (
             <ExperienceCard
@@ -158,6 +158,19 @@ export default function Home() {
             />
           ))}
         </div>
+      </section>
+
+      <section>
+        <h1 className={`font-semibold text-2xl mt-8 mb-4`}>Contact</h1>
+        <p className="text-gray-700 text-base mb-2">Want to work together?</p>
+        <form action="https://formspree.io/jimmyasyraf@gmail.com" method="POST" className="space-y-4">
+          <Input name="email" type="email" placeholder="Your email" />
+
+          <Textarea name="message" placeholder="Your message" />
+
+          <Button type="submit">Submit</Button>
+          <Input type="hidden" name="_next" value="https://hazimiasyraf.com" />
+        </form>
       </section>
     </div>
   );
