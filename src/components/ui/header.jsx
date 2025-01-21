5ec4f47d66a8534abe694e44d94175db6567510e
Hazimi Asyraf <jimmyasyraf@gmail.com>
2025-01-21 15:38:47 +0800

    Header transparency

--- src/components/ui/header.jsx
+++ src/components/ui/header.jsx
@@ -11,7 +11,7 @@ const spaceGrotesk = Space_Grotesk({
 export function Header() {
 
   return (
-    <nav className='sticky top-0 h-14 border-b bg-white z-50'>
+    <nav className='sticky top-0 h-14 border-b bg-white z-50 backdrop-blur bg-opacity-70'>
       <div className="max-w-2xl mx-auto h-14 flex items-center justify-between px-4">
         <div>
           <Link
